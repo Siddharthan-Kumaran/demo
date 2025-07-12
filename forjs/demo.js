e24b7bd630556a3f7e409321edcc6afd70d17bd8
Siddharthan <siddharthsiddharthan795@gmail.com>
2025-07-12 11:38:24 +0530

    hi

---
+++ forjs/demo.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 5;
+var f = "total";
+let c = a + b;
+console.log(a);
+console.log(b);
+console.log(f);
+console.log(c);
+function hello() {
+  console.log("welcome");
+}
+hello();
